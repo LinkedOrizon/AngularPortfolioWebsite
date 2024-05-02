33b868f014ee97c35aa59dd4b261f84c57a6ff0f
LinkedOrizon <kieranclark272@gmail.com>
2024-05-02 22:45:30 +1200

    Project structure created, Navigation and homepage elements implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from "./home/home.component";
+import { NavigationComponent } from "./navigation/navigation.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, CommonModule, HomeComponent, NavigationComponent]
 })
 export class AppComponent {
   title = 'AngularPortfolioWebsite';
