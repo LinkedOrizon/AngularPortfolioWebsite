990d38ea0a5acd638c0aef8d96b0dd6988a9e139
LinkedOrizon <kieranclark272@gmail.com>
2024-05-09 17:00:05 +1200

    Project component implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from "./home/home.component";
 import { NavigationComponent } from "./navigation/navigation.component";
+import { AboutComponent } from "./about/about.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CommonModule, HomeComponent, NavigationComponent]
+    imports: [RouterOutlet, CommonModule, HomeComponent, NavigationComponent, AboutComponent, ProjectsComponent]
 })
 export class AppComponent {
   title = 'AngularPortfolioWebsite';
